b9cda8183c8c9b9e8e1e69123e773b361919bb69
William Chong <me@williamchong.cloud>
2024-02-16 17:01:11 +0800

    💄 Simple UI structure for metadata display

--- web/app.vue
+++ web/app.vue
@@ -1,28 +1,63 @@
 <template>
   <div>
-    <input type="file" @change="onFileChange">
-    <br >
-    <h2>c2pa</h2>
-    <h3>Active manifest</h3>
-    <pre v-if="activeManifestString">
-      {{ activeManifestString }}
-    </pre>
-    <hr />
-    <h3>Manifest store</h3>
-    <pre v-if="manifestString">
-      {{ manifestString }}
-    </pre>
-    <hr />
-    <h2>Numbers</h2>
-    <h3>cid</h3>
-    <pre v-if="fileCid">
-      {{ fileCid }}
-    </pre>
-    <hr />
-    <h3>metadata</h3>
-    <pre v-if="numbersMetadataString">
-      {{ numbersMetadataString }}
-    </pre>
+    <div>
+      <input type="file" @change="onFileChange">
+      <br>
+      <video control ref="videoPreview"></video>
+    </div>
+    <div>
+      <h2>Video Metadata</h2>
+      <table>
+        <tr>
+          <td>Title</td>
+          <td>{{ c2paActiveManifest?.title }}</td>
+        </tr>
+        <tr>
+          <td>Published By</td>
+          <td>{{ c2paActiveManifest?.claimGenerator }}</td>
+        </tr>
+        <tr>
+          <td>FingerPrint</td>
+          <td>{{ fileCid }}</td>
+        </tr>
+      </table>
+    </div>
+    <div>
+      <h2>Authencity Metadata</h2>
+      <h3>c2pa</h3>
+      <div v-if="c2paActiveManifest">
+        Verified
+        <button @click="showC2paManifestStore = !showC2paManifestStore">
+          Show manifest store
+        </button>
+        <div v-if="showC2paManifestStore">
+          <h3>Manifest store</h3>
+          <pre>
+            {{ c2paManifestStoreString }}
+          </pre>
+        </div>
+      </div>
+      <div v-else>
+        Not verified
+      </div>
+      <hr />
+      <h3>Numbers</h3>
+      <div v-if="numbersMetadata">
+        Verified <a :href="`https://verify.numbersprotocol.io/?nid=${fileCid}`" target="_blank" rel="noopener">
+          View on Numbers
+        </a>
+        <button @click="showNumbersMetadata = !showNumbersMetadata">View raw data</button>
+        <div v-if="showNumbersMetadata">
+          <h4>Numbers metadata</h4>
+          <pre>
+            {{ numbersMetadataString }}
+          </pre>
+        </div>
+      </div>
+      <div v-else>
+        Not verified
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -31,10 +66,32 @@ import wasmSrc from 'c2pa/dist/assets/wasm/toolkit_bg.wasm?url';
 import workerSrc from 'c2pa/dist/c2pa.worker.js?url';
 import Hash from 'ipfs-only-hash';
 
-const manifestString = ref('')
-const activeManifestString = ref('')
+const c2paManifestStore = ref<any>(null)
 const fileCid = ref('')
-const numbersMetadataString = ref('')
+const numbersMetadata = ref<any>(null)
+const videoPreview = ref<HTMLInputElement | null>(null)
+
+const showC2paManifestStore = ref(false)
+const showNumbersMetadata = ref(false)
+
+const c2paActiveManifest = computed(() => {
+  return c2paManifestStore.value?.activeManifest
+})
+const c2paManifestStoreString = computed(() => {
+  return JSON.stringify(
+    c2paManifestStore.value,
+    (key, value) => key === 'parent' ? undefined : value,
+    2,
+  )
+});
+const numbersMetadataString = computed(() => {
+  return JSON.stringify(
+    numbersMetadata.value,
+    (key, value) => key === 'parent' ? undefined : value,
+    2,
+  )
+});
+
 let c2pa: C2pa | null = null
 
 onMounted(() => {
@@ -67,8 +124,7 @@ async function readFileCid(file: File) {
 
 async function fetchNumbersMetadata(cid: string) {
   const { data } = await useFetch(`https://api.numbersprotocol.io/api/v3/assets/${cid}`)
-  const metadata = data.value
-  numbersMetadataString.value = JSON.stringify(metadata, null, 2)
+  numbersMetadata.value = data.value
 }
 
 async function readC2pa(file: File) {
@@ -76,30 +132,22 @@ async function readC2pa(file: File) {
   try {
     const { manifestStore } = await c2pa.read(file);
     console.log('manifestStore', manifestStore);
-    manifestString.value = JSON.stringify(
-      manifestStore,
-      (key, value) => key === 'parent' ? undefined : value,
-      2,
-    )
-
-    // Get the active manifest
-    const activeManifest = manifestStore?.activeManifest;
-    console.log('activeManifest', activeManifest);
-    activeManifestString.value = JSON.stringify(
-      activeManifest,
-      (key, value) => key === 'parent' ? undefined : value,
-      2,
-    )
+    c2paManifestStore.value = manifestStore;
   } catch (err) {
     console.error('Error reading image:', err);
   }
 }
 
 async function onFileChange (event: Event) {
+  c2paManifestStore.value = null;
+  numbersMetadata.value = null;
+  if (videoPreview.value) videoPreview.value.src = '';
   if (!event?.target) { return }
   const files = (event.target as HTMLInputElement)?.files
   if (!files) { return }
   const [file] = files
+  const blobURL = URL.createObjectURL(file);
+  if (videoPreview.value) videoPreview.value.src = blobURL;
   await Promise.all([
     readC2pa(file),
     readFileCid(file),
